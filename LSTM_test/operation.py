e6a6c49f3db29acbecd5914499c3db76306917a7
yellowssun <hbsun98@gmail.com>
2022-05-25 20:39:53 +0900

    feat: edit load_data
    에러 데이터를 추가하기 위해 함수 load_data 수정

--- LSTM_test/operation.py
+++ LSTM_test/operation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.utils import to_categorical
+from keras.utils import to_categorical
 
 
 def get_points_angle(landmark_subset):
